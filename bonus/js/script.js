886290291098bbcfed95bada2f4ba2e545b16efa
ChristianPucci91 <pucci.christian91@gmail.com>
2020-12-05 17:15:03 +0100

    Milestone 6: aggiunto menu dropdown con scelta genere tra film o serie tv

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,12 +7,16 @@ var app = new Vue({
     logo:'img/logo0.png', // logo boolfix
     apiKey:'da0d46e7635a0896a9556496ca9aabfb',
     search:'', // v-model per cercare i film
-    movies:[],// array dei film
-    series:[], // array delle serie tv
     genreMovies:[], // array generi film
     genreSeries:[], // array generi Serie Tv
     actorsMovies:[], // array attori film
-    actorsSeries:[] // array attori Serie Tv
+    actorsSeries:[], // array attori Serie Tv
+    allFilmTv:[], //array unificato
+    showDropFilm:false,
+    showDropTv:false
+  },
+  mounted: function (){
+   this.sortPopularity();
   },
   methods: {// andiamo a richiedere all'API tutti i film e li associamo al nostro array movies con un mounted, non appena si crea l'istanza di Vue
     filter () {
@@ -23,18 +27,8 @@ var app = new Vue({
           query: this.search // associamo a query il valore di quello che andremo ad inserire
         }
       }).then(risp =>{
-        this.movies = risp.data.results;
+        this.allFilmTv = risp.data.results;
 
-        this.movies.forEach((item) => {
-
-          item.overview = this.overFlow(item.overview);
-
-          if (item.poster_path == null) {
-            item.poster_path = 'img/placeholder.png';
-          }else{
-            item.poster_path = `https://image.tmdb.org/t/p/w342/${item.poster_path}`;
-          }
-        });
         //per ogni item dell'array creo il link con il codice poster_path associato al film
       });
       axios.get('https://api.themoviedb.org/3/search/tv', {
@@ -44,8 +38,8 @@ var app = new Vue({
         }
       }).then(risp =>{
 
-        this.series = risp.data.results;
-        this.series.forEach((item) => {
+        this.allFilmTv = [...this.allFilmTv, ...risp.data.results]
+        this.allFilmTv.forEach((item) => {
 
           item.overview = this.overFlow(item.overview);
 
@@ -70,7 +64,6 @@ var app = new Vue({
           this.actorsMovies.push(risp.data.credits.cast[i]);
        }
        this.genreMovies = risp.data.genres;
-       console.log(this.genreMovies);
       });
       axios.get('https://api.themoviedb.org/3/tv/' + id + '?api_key=' + this.apiKey + '&append_to_response=credits').then(risp =>{
         // vogliamo solo i primi 5 attori quindi pushamo nell'array i primi 5 risultati
@@ -89,10 +82,63 @@ var app = new Vue({
     imgPlaceholder:function (e) {  // quando non si trovano bandiere andrà in errore e aggiungerà questa di default
       e.target.src = "img/world.png"
     },
-    overFlow(item) {
+    overFlow(item) { // funzione per accorciare il testo superati i 200 caratteri
       if (item.length > 200){
         return item = item.slice(0,200).concat('...');
       };
-    }, // funzione per accorciare il testo superati i 200 caratteri
+    },
+    sortPopularity(){  // funzione per prendere film e serie più popolari
+      axios.get('https://api.themoviedb.org/3/discover/movie?&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', {
+        params:{
+          api_key: this.apiKey
+        }
+      }).then(risp =>{
+        this.allFilmTv = risp.data.results;
+
+      });
+      axios.get('https://api.themoviedb.org/3/discover/tv?&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', {
+        params:{
+          api_key: this.apiKey
+        }
+      }).then(risp =>{
+        this.allFilmTv = [...this.allFilmTv,...risp.data.results]
+        this.allFilmTv.forEach((item) => {
+
+          if (item.poster_path == null) {
+            item.poster_path = 'img/placeholder.png';
+          }
+          else{
+            item.poster_path = `https://image.tmdb.org/t/p/w342${item.poster_path}`;
+          }
+        });
+      });
+    },
+    bygenreFilm(id){ //MILESTONE 6 scelta per genere a seconda se si sceglie un film o una serie tv
+      axios.get('https://api.themoviedb.org/3/discover/movie?api_key='+ this.apiKey + '&with_genres=' + id).then(risp =>{
+        this.allFilmTv = risp.data.results;
+        this.allFilmTv.forEach((item) => {
+
+          if (item.poster_path == null) {
+            item.poster_path = 'img/placeholder.png';
+          }else{
+            item.poster_path = `https://image.tmdb.org/t/p/w342/${item.poster_path}`;
+          }
+        });
+      })
+    },
+    bygenreTv(id){
+
+      axios.get('https://api.themoviedb.org/3/discover/tv?api_key='+ this.apiKey + '&with_genres=' + id).then(risp =>{
+        this.allFilmTv = risp.data.results;
+        this.allFilmTv.forEach((item) => {
+
+          if (item.poster_path == null) {
+            item.poster_path = 'img/placeholder.png';
+          }else{
+            item.poster_path = `https://image.tmdb.org/t/p/w342/${item.poster_path}`;
+          }
+        });
+      })
+    }
   }
 });
